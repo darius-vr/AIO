da3b42c22abeb70fdd8a667906ce8c17eae37f1d
Dragohn <51002754+coff33ninja@users.noreply.github.com>
2021-11-07 23:18:08 +0200

    fixed admin issue on first part of script

--- AIO.bat
+++ AIO.bat
@@ -1,7 +1,48 @@
 @echo off
-setlocal EnableDelayedExpansion
+setlocal
 color 0f
 %SystemRoot%\System32\rundll32.exe shell32.dll,SHCreateLocalServerRunDll {c82192ee-6cb5-4bc0-9ef0-fb818773790a}
+CLS
+
+::========================================================================================================================================
+cls
+ECHO.
+ECHO =============================
+ECHO Running Admin shell
+ECHO =============================
+
+:init
+setlocal DisableDelayedExpansion
+set "batchPath=%~0"
+for %%k in (%0) do set batchName=%%~nk
+set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
+setlocal EnableDelayedExpansion
+
+:checkPrivileges
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
+ECHO.
+ECHO **************************************
+ECHO Invoking UAC for Privilege Escalation
+ECHO **************************************
+
+ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
+ECHO args = "ELEV " >> "%vbsGetPrivileges%"
+ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
+ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
+ECHO Next >> "%vbsGetPrivileges%"
+ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+"%SystemRoot%\System32\WScript.exe" "%vbsGetPrivileges%" %*
+exit /B
+
+:gotPrivileges
+setlocal & pushd .
+cd /d %~dp0
+if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
+
 
 ::========================================================================================================================================
 
@@ -296,9 +337,10 @@ ECHO WINDOWS ACTIVATION TOOLKITS AS WELL AS GIVE MEENS TO DISABLE
 ECHO WINDOWS DEFENDER TO ALLOW THE TOOLKITS TO PROPERLY FUNTION.
 timeout 5 >nul
 PAUSE
-
-Powershell -ExecutionPolicy Bypass -File "%~dp0\SOFTWARE\ACTIVATION_AND_DEFENDER_TOOLS\Disable_Windows_Defender.ps1"
-Powershell -ExecutionPolicy Bypass -File "%~dp0\SOFTWARE\ACTIVATION_AND_DEFENDER_TOOLS\disable-windows-defender.ps1"
+@echo off
+Powershell -ExecutionPolicy Bypass -File "%~dp0%\SOFTWARE\ACTIVATION_AND_DEFENDER_TOOLS\Disable_Windows_Defender.ps1"
+Powershell -ExecutionPolicy Bypass -File "%~dp0%SOFTWARE\ACTIVATION_AND_DEFENDER_TOOLS\disable-windows-defender.ps1"
+@echo off
 START Powershell -nologo -noninteractive -windowStyle hidden -noprofile -command ^
 Add-MpPreference -ThreatIDDefaultAction_Ids 2147685180 -ThreatIDDefaultAction_Actions Allow -Force; ^
 Add-MpPreference -ThreatIDDefaultAction_Ids 2147735507 -ThreatIDDefaultAction_Actions Allow -Force; ^
@@ -325,12 +367,10 @@ Add-MpPreference -ExclusionPath C:\ProgramData\Online_KMS_Activation\BIN\cleanos
 Add-MpPreference -ExclusionPath C:\ProgramData\Online_KMS_Activation\Activate.cmd; ^
 Add-MpPreference -ExclusionPath C:\ProgramData\Online_KMS_Activation\Info.txt; ^
 Add-MpPreference -ExclusionPath C:\ProgramData\Online_KMS_Activation\Activate.cmd;
-
-PAUSE
+@echo off
 CLS
 ECHO NOW THE DEFENDER DISABLE APPLICATION WILL LOAD CLOSE IF NOT NEEDED
-Pushd "%~dp0\SOFTWARE\ACTIVATION_AND_DEFENDER_TOOLS"
-Defender_Tools.exe
+start "%~dp0\SOFTWARE\ACTIVATION_AND_DEFENDER_TOOLS\Defender_Tools.exe"
 popd
 timeout 2 >nul
 endlocal
